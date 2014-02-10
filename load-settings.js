c647ac8d393fb515976a1517e5e163e922e3a7ae
Wade P <darth.wade@live.com>
2014-02-10 17:12:03 +0100

    Initial commit

--- load-settings.js
+++ load-settings.js
@@ -2,7 +2,7 @@
 module.exports = function(grunt, options) {
   grunt.config.set('environment', options);
 
-  var EnvironmentManager = require('./utils/EnvironmentManager');
+  var EnvironmentManager = require('./utils/EnvironmentManager.js');
   var manager = new EnvironmentManager(grunt, options);
   manager.injectEnvironmentSettings();
 };
